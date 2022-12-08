8d3444f8bd6a252ad9ad8903d4be2e9df9b2194a
AliRazavi <razavi.ali.1998@gmail.com>
2022-12-08 22:27:17 +0330

    feat: user create functionality

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -1,10 +1,46 @@
-const userList = (req, res, next) => {
-  res.send({
+const userModel = require('../models/UserModel')
+
+const userList = async (req, res, next) => {
+  const users = await userModel.find({})
+  res.status(200).send({
     success: 200,
     message: 'لیست کاربران دریافت شد .',
+    result: users,
   })
+}
+
+const addUser = async (req, res, next) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      wallet,
+      username,
+      password,
+      phoneNumber,
+    } = req.body
+    const user = await new userModel({
+      firstName,
+      lastName,
+      email,
+      wallet,
+      username,
+      password,
+      phoneNumber,
+    })
+    await user.save()
+    res.status(201).send({
+      success: 201,
+      message: 'کاربر جدید اضافه شد .',
+      result: user,
+    })
+  } catch (error) {
+    next(error)
+  }
 }
 
 module.exports = {
   userList,
+  addUser,
 }
--- app/routes/users.js
+++ app/routes/users.js
@@ -2,5 +2,5 @@ const express = require('express')
 const userRouter = express.Router()
 const userContoller = require('../controllers/userController')
 userRouter.get('/', userContoller.userList)
-
+userRouter.post('/', userContoller.addUser)
 module.exports = userRouter
